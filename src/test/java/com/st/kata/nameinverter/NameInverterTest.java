58c6456d5cf9ede820b583a7ef366f0a8f295baf
PeggyBrown <ola.qnysz@gmail.com>
2020-03-17 18:29:48 +0100

    Project setup

---
+++ src/test/java/com/st/kata/nameinverter/NameInverterTest.java
@@ -0,0 +1,12 @@
+package com.st.kata.nameinverter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NameInverterTest {
+
+    @Test
+    void shouldThrowExceptionWhenNullProvided() {
+        Assertions.assertThat(true).isEqualTo(true); //xD
+    }
+}
